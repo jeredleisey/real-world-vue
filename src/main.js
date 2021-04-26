991214a5bda52ce5fc6dc0e4ee65850de675a44c
jered <jered.leisey@gmail.com>
2021-04-26 12:17:43 -0400

    Completed Forms VueMastery Course

--- src/main.js
+++ src/main.js
@@ -2,8 +2,35 @@ import { createApp, reactive } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
 import 'nprogress/nprogress.css'
 
 const GStore = reactive({ flashMessage: '' })
 
-createApp(App).use(store).use(router).provide('GStore', GStore).mount('#app')
+// 1. This code sets up automatic Component registration using the require.context function
+const requireComponent = require.context(
+  './components',
+  false,
+  /Base[A-Z]\w+\.(vue|js)$/
+)
+
+const app = createApp(App)
+
+// 2. This is the heart of setting up the required components so they don't have to be registered each time
+requireComponent.keys().forEach((fileName) => {
+  const componentConfig = requireComponent(fileName)
+
+  const componentName = upperFirst(
+    camelCase(
+      fileName
+        .split('/')
+        .pop()
+        .replace(/\.\w+$/, '')
+    )
+  )
+
+  app.component(componentName, componentConfig.default || componentConfig)
+})
+
+app.use(store).use(router).provide('GStore', GStore).mount('#app')
